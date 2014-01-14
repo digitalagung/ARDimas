d78c7fd34fe488a65a6c2f97197c956d888efab8
unknown <dynastymasra@gmail.com>
2014-01-14 10:04:47 +0700

    first commit

---
+++ src/com/dynastymasra/augmented/Spalsh.java
@@ -0,0 +1,15 @@
+package com.dynastymasra.augmented;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Spalsh extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
